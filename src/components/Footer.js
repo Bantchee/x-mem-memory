39600457712fede89a62a3cfbbc37853bc5dd0a0
Jar'Zeno <jarzenojarrett@gmail.com>
2023-01-09 07:44:56 -0800

    Style footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,25 @@
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Footer = (props) => {
     return (
-        <div>
-            <h1>
+        <div
+            className="flex justify-center content-center bg-yellow-400 gap-2 pt-6 pb-6"
+        >
+            <h1
+                className=" text-4xl font-bold ">
                 Made By J. Steven Jarrett
             </h1>
             <a 
                 href="https://github.com/Bantchee"
                 rel="noreferrer"
                 target="_blank"
+                className="flex content-center"
             >
-                <p>GitHub Temp Text</p>
+                <FontAwesomeIcon 
+                    icon={faGithub}
+                    className="w-12 h-12"
+                />
             </a>
         </div>
     );
