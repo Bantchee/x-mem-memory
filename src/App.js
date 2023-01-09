bc389ff26251d3b78c0822da776c8903136d390e
Jar'Zeno <jarzenojarrett@gmail.com>
2023-01-09 07:23:13 -0800

    Create footer comp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header.js';
 import Main from './components/Main.js';
+import Footer from './components/Footer.js'
 
 const App = () => {
   return (
     <div>
       <Header />
       <Main />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+const Footer = (props) => {
+    return (
+        <div>
+            <h1>
+                Made By J. Steven Jarrett
+            </h1>
+            <a 
+                href="https://github.com/Bantchee"
+                rel="noreferrer"
+                target="_blank"
+            >
+                <p>GitHub Temp Text</p>
+            </a>
+        </div>
+    );
+}
+
+export default Footer;
