7f941a34e3158e3c76466146bd043a06afa0c954
Jar'Zeno <jarzenojarrett@gmail.com>
2023-01-13 12:07:47 -0800

    Add footer github link test

--- src/components/__tests__/Footer.test.js
+++ src/components/__tests__/Footer.test.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Footer from "../Footer";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 describe("Footer component", () => {
     test("renders site footer", () => {
         const { container } = render(<Footer />);
         expect(container).toMatchSnapshot();
     });
-});
 
-// test github link
+    test("renders link to github", () => {
+        render(<Footer />);
+        const link = screen.getByRole("link");
+
+        expect(link.getAttribute("href")).toBe("https://github.com/Bantchee");
+    });
+});
