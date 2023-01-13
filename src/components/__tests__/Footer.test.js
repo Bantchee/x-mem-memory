526dedd5e1fcb2ee2aca94e54804e48fb5f6138f
Jar'Zeno <jarzenojarrett@gmail.com>
2023-01-13 12:08:44 -0800

    Remove unused import

--- src/components/__tests__/Footer.test.js
+++ src/components/__tests__/Footer.test.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Footer from "../Footer";
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 
 describe("Footer component", () => {
     test("renders site footer", () => {
