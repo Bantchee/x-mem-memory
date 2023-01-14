f42e0b979c18551d3b035a4b8079cfc1b5068f74
Jar'Zeno <jarzenojarrett@gmail.com>
2023-01-14 08:58:48 -0800

    Main testing

--- src/components/__tests__/Main.test.js
+++ src/components/__tests__/Main.test.js
@@ -1,24 +1,35 @@
-// Test List:
-// snapshot
-// handleCardClick()
-    // click card and test if handleCardClick() has been called once
-// resetCard()
-    // click card and test state of each card hasBeenClicked = false
-// shuffleCard()
-    // click card
-        // Test :
-            // Same num of cards in deck
-            // prevState of Cards is different from currentState of Cards
-// replaceCardByName() *Name should maybe be changed*
-    // click card
-        // Test :
-            // If Card state property hasBeenClicked = true;
-// increaseScore()
-    // click card 
-        // Test :
-            // If current score increases
-            // If best score increases
-// resetCurrentScore()
-    // click card (twice):
-        // Test : 
-            // IF current score is reset to zero if card has already been clicked before
+import Main from "../Main";
+import { render, screen, act } from "@testing-library/react"
+import "@testing-library/jest-dom"
+import userEvent from "@testing-library/user-event";
+// import { act } from "react-dom/test-utils";
+
+describe("Main Component", () => {
+    test("renders", () => {
+        const { container } = render(<Main />);
+        expect(container).toMatchSnapshot();
+    });
+
+
+});
+
+describe("Card Clicked Once", () => {
+    test("Current Score increases", () => {
+        render(<Main />);
+        const cards = screen.getAllByRole("button");
+        const currentScore = screen.getByText(/Current Score:/i);
+
+        userEvent.click(cards[0]);
+        expect(currentScore.textContent).toMatch(/Current Score: 1/i);
+    });
+
+    test("Best Score increases", () => {
+        render(<Main />);
+        const cards = screen.getAllByRole("button");
+        const bestScore = screen.getByText(/Best Score:/i);
+
+        userEvent.click(cards[0]);
+        
+        expect(bestScore.textContent).toMatch(/Best Score: 1/i);
+    });
+});
